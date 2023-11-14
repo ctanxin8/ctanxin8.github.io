a2b9a5e8f722783a72b3a8b99a6510c29c2a61d2
ctanxin8 <133303086+ctanxin8@users.noreply.github.com>
2023-11-14 18:34:15 +0800

    Site updated: 2023-11-14 18:34:14

--- js/txmap.js
+++ js/txmap.js
@@ -3,7 +3,7 @@ $.ajax({
     type: 'get',
     url: 'https://apis.map.qq.com/ws/location/v1/ip',
     data: {
-        key: 'OZRBZ-VAAK5-TXLIU-I6IOI-YKEX6-R2B6C',
+        key: '你的key',
         output: 'jsonp',
     },
     dataType: 'jsonp',
@@ -29,7 +29,7 @@ function getDistance(e1, n1, e2, n2) {
 
 function showWelcome() {
 
-    let dist = getDistance(28.746762,104.623761, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
+    let dist = getDistance(113.34499552, 23.15537143, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
     let pos = ipLoacation.result.ad_info.nation;
     let ip;
     let posdesc;
